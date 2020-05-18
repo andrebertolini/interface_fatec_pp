acd6f1cd4fca6a98e65106bd9930399ec4402cac
Andre Luiz Bertolini Ferreira <andre.bertolini@bonmart.com>
2020-05-18 16:33:15 -0300

    mudanças

--- src/script.js
+++ src/script.js
@@ -6,10 +6,44 @@ function menu() {
     now = new Date;    
     mes = now.getMonth()+1;     
     if (mes != 1) {
-        document.getElementById("divMatricula").style.backgroundColor = "red";
+        document.getElementById("matricula").style.backgroundColor = "red";
     } else {
-        document.getElementById("divMatricula").style.backgroundColor = "green";
+        document.getElementById("matricula").style.backgroundColor = "green";
     }
 
 }
+
+function agenda() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("agenda").style.display = "flex";
+
+}
+
+function ihc() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("ihc").style.display = "flex";
+
+}
+
+function con() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("con").style.display = "flex";
+
+}
+
+function historico() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("historico").style.display = "flex";
+
+}
+
+function horario() {
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("horario").style.display = "flex";
+
+}
+
+
+
+
 
